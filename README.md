5e0f3c045a37c0c35a6fbaa38b35e36779992da4
Junior Cabriales-Rodriguez <jacksonwolfd20@gmail.com>
2022-08-11 17:47:57 -0400

    Added readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Quiz-Theory
+Quiz
+Deployable Link - > https://jacksonwolfd20.github.io/Quiz-Theory/
+
+Description:
+This gives a simple quiz about simple questions as a proof of concept of the code
+
+Images:
+![Timer for the page](assets\opera_1EytQMT9vO.png)
+![Start of the quiz](assets\opera_csGw3kvXbu.png)
+![Score being demonstrated](assets\opera_JWoRdvoaUO.png)
--- assets/script.js
+++ assets/script.js
@@ -50,73 +50,122 @@ startButton.setAttribute("style", " width: 100%; font-size: x-large; background-
 
 //start button interaction
 function Start() {
-Timer.textContent = "The Quiz";
+    Timer.textContent = "The Quiz";
 }
 
-function highScore(){
+function highScore() {
     if (HighScore < realcounter) {
         localStorage.setItem("count", HighScore);
-        displayScore.textContent =  "You have a new highscore of " + realcounter + "/5"
-  }else{
-      displayScore.textContent = "your score hasnt beat" + highScore + "/5"
-  }
+        displayScore.textContent = "You have a new highscore of " + realcounter + "/5"
+    } else {
+        displayScore.textContent = "your score hasnt beat" + highScore + "/5"
+    }
 
 
 }
 
 
-function failState (){
+function failState() {
+    if (timeLeft === 0 || timeLeft < 1)
         buttons.removeChild(bt1);
-        buttons.removeChild(bt2);
-        buttons.removeChild(bt3);
-        buttons.removeChild(bt4);
+    buttons.removeChild(bt2);
+    buttons.removeChild(bt3);
+    buttons.removeChild(bt4);
+    Timer.textContent = 'Quiz is finished';
+
+    Question.textContent = "your score is " + realcounter + "/5";
+    buttons.appendChild(displayScore);
+    clearInterval(timeInterval);
+
+
+}
+function failState2() {
+    if (timeLeft === 0 || timeLeft < 1)
         buttons.removeChild(bt12);
-        buttons.removeChild(bt22);
-        buttons.removeChild(bt32);
-        buttons.removeChild(bt42);
+    buttons.removeChild(bt22);
+    buttons.removeChild(bt32);
+    buttons.removeChild(bt42);
+    Timer.textContent = 'Quiz is finished';
+
+    Question.textContent = "your score is " + realcounter + "/5";
+    buttons.appendChild(displayScore);
+    clearInterval(timeInterval);
+
+
+}
+function failState3() {
+    if (timeLeft === 0 || timeLeft < 1)
         buttons.removeChild(bt13);
-        buttons.removeChild(bt23);
-        buttons.removeChild(bt33);
-        buttons.removeChild(bt43);
+    buttons.removeChild(bt23);
+    buttons.removeChild(bt33);
+    buttons.removeChild(bt43);
+    Timer.textContent = 'Quiz is finished';
+
+    Question.textContent = "your score is " + realcounter + "/5";
+    buttons.appendChild(displayScore);
+    clearInterval(timeInterval);
+
+
+}
+
+function failState4() {
+    if (timeLeft === 0 || timeLeft < 1)
         buttons.removeChild(bt14);
-        buttons.removeChild(bt24);
-        buttons.removeChild(bt34);
-        buttons.removeChild(bt44);
+    buttons.removeChild(bt24);
+    buttons.removeChild(bt34);
+    buttons.removeChild(bt44);
+    Timer.textContent = 'Quiz is finished';
+
+    Question.textContent = "your score is " + realcounter + "/5";
+    buttons.appendChild(displayScore);
+    clearInterval(timeInterval);
+
+
+}   
+function failState5() {
+    if (timeLeft === 0 || timeLeft < 1)
         buttons.removeChild(bt15);
-        buttons.removeChild(bt25);
-        buttons.removeChild(bt35);
-        buttons.removeChild(bt45);
-      } 
-    
+    buttons.removeChild(bt25);
+    buttons.removeChild(bt35);
+    buttons.removeChild(bt45);
+    Timer.textContent = 'Quiz is finished';
+
+    Question.textContent = "your score is " + realcounter + "/5";
+    buttons.appendChild(displayScore);
+    clearInterval(timeInterval);
+
+
+}
+
 
 
 //the start button
-startButton.addEventListener("click", function() {
+startButton.addEventListener("click", function () {
     firstQuestion()
 
     var timeInterval = setInterval(function () {
 
-      if (timeLeft > 1) {
+        if (timeLeft > 1) {
 
-        Timer.textContent = timeLeft + ' seconds remaining';
+            Timer.textContent = timeLeft + ' seconds remaining';
 
-        timeLeft--;
-      } else if (timeLeft === 1) {
+            timeLeft--;
+        } else if (timeLeft === 1) {
 
-        Timer.textContent = timeLeft + ' second remaining';
-        timeLeft--;
-      } else  {
-
-        failState ()
-        Timer.textContent = 'Quiz is finished';
-
-    Question.textContent = "your score is " + realcounter +"/5";
-    buttons.appendChild(displayScore);
-        clearInterval(timeInterval);
-        
+            Timer.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
 
 
-      }
+            Timer.textContent = 'Quiz is finished';
+
+            Question.textContent = "your score is " + realcounter + "/5";
+            buttons.appendChild(displayScore);
+            clearInterval(timeInterval);
+
+
+
+        }
     }, 1000);
 });
 
@@ -126,54 +175,54 @@ Start();
 //first question
 function firstQuestion() {
 
-Question.textContent = "What ingredients are in a cake"
+    Question.textContent = "What ingredients are in a cake"
 
-buttons.removeChild(startButton)
-bt1.textContent = "Flour"
-bt2.textContent = "Steak"
-bt3.textContent = "Soda"
-bt4.textContent = "Plastic"
+    buttons.removeChild(startButton)
+    bt1.textContent = "Flour"
+    bt2.textContent = "Steak"
+    bt3.textContent = "Soda"
+    bt4.textContent = "Plastic"
 
-buttons.appendChild(bt1);
-buttons.appendChild(bt2);
-buttons.appendChild(bt3);
-buttons.appendChild(bt4);
+    buttons.appendChild(bt1);
+    buttons.appendChild(bt2);
+    buttons.appendChild(bt3);
+    buttons.appendChild(bt4);
 
-bt1.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
-bt2.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
-bt3.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
-bt4.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: block;");
+    bt1.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
+    bt2.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
+    bt3.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
+    bt4.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: block;");
 
 
 
 };
 
-bt2.addEventListener("click", function() {
+bt2.addEventListener("click", function () {
     bt2.textContent = "Wrong"
     bt2.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
+    timeLeft = timeLeft - 10;
     failState();
 
 })
-bt3.addEventListener("click", function() {
+bt3.addEventListener("click", function () {
     bt3.textContent = "Wrong"
     bt3.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
+    timeLeft = timeLeft - 10;
     failState();
 })
-bt4.addEventListener("click", function() {
+bt4.addEventListener("click", function () {
     bt4.textContent = "Wrong"
     bt4.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
+    timeLeft = timeLeft - 10;
     failState();
 })
 
 //first set of questions
 
-bt1.addEventListener("click", function(){
-    
+bt1.addEventListener("click", function () {
+
     Question.textContent = "What does a dog not have"
-    
+
     buttons.removeChild(bt1)
     buttons.removeChild(bt2)
     buttons.removeChild(bt3)
@@ -188,7 +237,7 @@ bt1.addEventListener("click", function(){
     bt22.textContent = "Scales"
     bt32.textContent = "Tails"
     bt42.textContent = "Teeth"
-    
+
     bt12.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
     bt22.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
     bt32.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
@@ -198,33 +247,36 @@ bt1.addEventListener("click", function(){
 
     counter.textContent = realcounter + "/5"
 
-} )
+})
 
 
-bt12.addEventListener("click", function() {
+bt12.addEventListener("click", function () {
     bt12.textContent = "Wrong"
     bt12.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
+    timeLeft = timeLeft - 10;
+    failState2();
 })
-bt32.addEventListener("click", function() {
+bt32.addEventListener("click", function () {
     bt32.textContent = "Wrong"
     bt32.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
+    timeLeft = timeLeft - 10;
+    failState2();
 })
-bt42.addEventListener("click", function() {
+bt42.addEventListener("click", function () {
     bt42.textContent = "Wrong"
     bt42.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
+    timeLeft = timeLeft - 10;
+    failState2();
 })
 
 //second set of questions
 
-bt22.addEventListener("click", function(){
-    
+bt22.addEventListener("click", function () {
+
     Question.textContent = "Which animal live in the water"
-    
+
     buttons.removeChild(bt12)
-    buttons.removeChild(bt22) 
+    buttons.removeChild(bt22)
     buttons.removeChild(bt32)
     buttons.removeChild(bt42)
 
@@ -237,7 +289,7 @@ bt22.addEventListener("click", function(){
     bt23.textContent = "Pig"
     bt33.textContent = "Cow"
     bt43.textContent = "Shark"
-    
+
     bt13.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
     bt23.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
     bt33.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
@@ -247,34 +299,34 @@ bt22.addEventListener("click", function(){
 
     counter.textContent = realcounter + "/5"
 
-} )
+})
 
 
-bt13.addEventListener("click", function() {
+bt13.addEventListener("click", function () {
     bt13.textContent = "Wrong"
     bt13.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
-    failState ()
+    timeLeft = timeLeft - 10;
+    failState3()
 })
-bt33.addEventListener("click", function() {
+bt33.addEventListener("click", function () {
     bt33.textContent = "Wrong"
     bt33.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
-    failState ()
+    timeLeft = timeLeft - 10;
+    failState3()
 })
-bt43.addEventListener("click", function() {
+bt43.addEventListener("click", function () {
     bt43.textContent = "Wrong"
     bt43.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
-    failState ()
+    timeLeft = timeLeft - 10;
+    failState3()
 })
 
 //third set of questions
 
-bt43.addEventListener("click", function(){
-    
+bt43.addEventListener("click", function () {
+
     Question.textContent = "What animal doesent live in the water"
-    
+
     buttons.removeChild(bt13)
     buttons.removeChild(bt23)
     buttons.removeChild(bt33)
@@ -289,7 +341,7 @@ bt43.addEventListener("click", function(){
     bt24.textContent = "Dog"
     bt34.textContent = "Whale"
     bt44.textContent = "Shrimp"
-    
+
     bt14.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
     bt24.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
     bt34.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
@@ -299,34 +351,34 @@ bt43.addEventListener("click", function(){
 
     counter.textContent = realcounter + "/5"
 
-} )
+})
 
 
-bt14.addEventListener("click", function() {
+bt14.addEventListener("click", function () {
     bt14.textContent = "Wrong"
     bt14.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
-    failState ()
+    timeLeft = timeLeft - 10;
+    failState4()
 })
-bt34.addEventListener("click", function() {
+bt34.addEventListener("click", function () {
     bt34.textContent = "Wrong"
     bt34.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
-    failState ()
+    timeLeft = timeLeft - 10;
+    failState4()
 })
-bt44.addEventListener("click", function() {
+bt44.addEventListener("click", function () {
     bt44.textContent = "Wrong"
     bt44.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
-    failState ()
+    timeLeft = timeLeft - 10;
+    failState4()
 })
 
 //fourth set of questions
 
-bt24.addEventListener("click", function(){
-    
+bt24.addEventListener("click", function () {
+
     Question.textContent = "What animal can fly"
-    
+
     buttons.removeChild(bt14)
     buttons.removeChild(bt24)
     buttons.removeChild(bt34)
@@ -341,7 +393,7 @@ bt24.addEventListener("click", function(){
     bt25.textContent = "Dog"
     bt35.textContent = "Whale"
     bt45.textContent = "Monkey"
-    
+
     bt15.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
     bt25.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
     bt35.setAttribute("style", " width: 100%; font-size: x-large; background-color: #33393f; color: #e866ec; border-radius: 15px; display: inline-block;");
@@ -351,40 +403,40 @@ bt24.addEventListener("click", function(){
 
     counter.textContent = realcounter + "/5"
 
-} )
+})
 
 
-bt25.addEventListener("click", function() {
+bt25.addEventListener("click", function () {
     bt25.textContent = "Wrong"
     bt25.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
-    failState ()
+    timeLeft = timeLeft - 10;
+    failState5()
 })
-bt35.addEventListener("click", function() {
+bt35.addEventListener("click", function () {
     bt35.textContent = "Wrong"
     bt35.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
-    failState ()
+    timeLeft = timeLeft - 10;
+    failState5()
 })
-bt45.addEventListener("click", function() {
+bt45.addEventListener("click", function () {
     bt45.textContent = "Wrong"
     bt45.setAttribute("style", " width: 100%; font-size: x-large; background-color: #ff0000; color: #ffffff; border-radius: 15px; display: inline-block;")
-    timeLeft = timeLeft-10;
-    failState ()
+    timeLeft = timeLeft - 10;
+    failState5()
 })
 
 //fifth set of questions
 
-bt15.addEventListener("click", function(){
-    
+bt15.addEventListener("click", function () {
+
     Question.textContent = "Test is done"
-    
+
     buttons.removeChild(bt15)
     buttons.removeChild(bt25)
     buttons.removeChild(bt35)
     buttons.removeChild(bt45)
 
-    timeLeft = 0 ;
+    timeLeft = 0;
 
     realcounter = realcounter + 1
 
@@ -392,7 +444,7 @@ bt15.addEventListener("click", function(){
 
 
 
-    
+
 })
-      
+
 
